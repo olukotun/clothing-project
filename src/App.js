7a57dc2a2707a09fea7ac779aaa751867f3ffcbc
olukotuno <oludamilareolukotun9@gmail.com>
2021-05-24 19:29:22 -0400

    update menu item to navigate with higher order component and also use the spread operator to spread the attribute in directory compoenent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {BrowserRouter as Router,Switch, Route} from 'react-router-dom'
+
+
+// history pros perform the same as Link props.history.push('/topic')
+const HatsPage =()=>(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
-    <div >
-      <HomePage />
+    <div>
+      <Router>
+        <Switch>
+          <HomePage />
+          <Route exact path="/new" component={HatsPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import MenuItem from '../menu-items/menu-item.component';
-import './directory.style.scss'
+import './directory.style.scss';
 
 class Directory extends Component {
   constructor(props) {
@@ -44,16 +44,14 @@ class Directory extends Component {
     };
   }
   render() {
-      const {sections} = this.state;
-    return(
-    <div className="directory-menu">
-      {
-      sections.map(({ title, imageUrl, id, size }) => (
-        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
-      ))
-      }
-    </div>
-    )
+    const { sections } = this.state;
+    return (
+      <div className="directory-menu">
+        {sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 export default Directory;
