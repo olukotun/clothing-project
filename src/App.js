5f380a85eae81051b437da36178a1feb5d5b78a5
olukotuno <oludamilareolukotun9@gmail.com>
2021-05-25 18:47:42 -0400

    added header component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import {BrowserRouter as Router,Switch, Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 // history pros perform the same as Link props.history.push('/topic')
+
 function App() {
   return (
     <div>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route  path="/shop" component={ShopPage} />
-        </Switch>
-      </Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+// const Home = (props) => {
+//   console.log(props);
+//   return (
+//     <div className="">
+//       <Link to="/topics">Topics</Link>
+//       <button onClick={() => props.history.push('/topics')}>Topics</button>
+//       <h1>HOME PAGE</h1>
+//     </div>
+//   );
+// };
+// const Topic = (props) => {
+//   console.log(props);
+//   return (
+//     <div className="">
+//       <h1>TOPIC PAGE</h1>
+//     </div>
+//   );
+// };
+// const TopicsList = () => (
+//   <div className="">
+//     <h1>TOPIC LIST PAGE</h1>
+//   </div>
+// );
+// const TopicDetails = (props) => {
+//   console.log(props);
+//   return (
+//     <div className="">
+//       <h1>TOPIC DETAILS PAGE: {props.match.params.topicId}</h1>
+//     </div>
+//   );
+// };
