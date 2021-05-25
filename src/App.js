ed41c2f11216ffc0db5859fbbe4383effb36933f
olukotuno <oludamilareolukotun9@gmail.com>
2021-05-25 00:09:50 -0400

    update app with collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import {BrowserRouter as Router,Switch, Route} from 'react-router-dom'
-
+import ShopPage from './pages/shop/shop.component';
 
 // history pros perform the same as Link props.history.push('/topic')
-const HatsPage =()=>(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
-
 function App() {
   return (
     <div>
       <Router>
         <Switch>
-          <HomePage />
-          <Route exact path="/new" component={HatsPage} />
+          <Route exact path="/" component={HomePage} />
+          <Route  path="/shop" component={ShopPage} />
         </Switch>
       </Router>
     </div>
